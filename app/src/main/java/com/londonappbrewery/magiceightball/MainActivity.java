4a41b12b321d2e34d94e2afe6c657f55b6755208
Ryan Adriano <ryan.adriano.910@gmail.com>
2018-01-05 07:39:26 -0600

    Initial revision

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,31 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGetAnswer;
+    private ImageView imgEightBall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setControlRefs();
+        setControlEvents();
     }
+
+    private void setControlRefs(){
+        btnGetAnswer = findViewById(R.id.btnGetAnswer);
+        imgEightBall = findViewById(R.id.imgEightBall);
+    }
+
+    private void setControlEvents()
+    {
+
+    }
+
 }
