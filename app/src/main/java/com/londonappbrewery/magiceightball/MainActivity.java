63e66c646ca2d1eb569c34bc3f7a42958e08d21b
Ryan Adriano <ryan.adriano.910@gmail.com>
2018-01-05 21:22:34 -0600

    Added images and final code for magic 8 ball random number

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,13 +2,23 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnGetAnswer;
     private ImageView imgEightBall;
+    private int[] mEightBallImages = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setControlRefs();
-        setControlEvents();
     }
 
-    private void setControlRefs(){
+    private void setControlRefs() {
         btnGetAnswer = findViewById(R.id.btnGetAnswer);
         imgEightBall = findViewById(R.id.imgEightBall);
+
+        btnGetAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(4);
+
+                imgEightBall.setImageResource(mEightBallImages[randomNumber]);
+            }
+        });
     }
-
-    private void setControlEvents()
-    {
-
-    }
-
 }
